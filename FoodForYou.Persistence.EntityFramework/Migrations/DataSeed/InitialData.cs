d2b3145c1096f28881405280e3540eb6e5f20d02
Paulo <pp.10.b.ct@gmail.com>
2021-02-07 21:06:37 +0000

    Added Default Employee

--- FoodForYou.Persistence.EntityFramework/Migrations/DataSeed/InitialData.cs
+++ FoodForYou.Persistence.EntityFramework/Migrations/DataSeed/InitialData.cs
@@ -1,4 +1,5 @@
-﻿using FoodForYou.Persistence.EntityFramework.Entities;
+﻿using System;
+using FoodForYou.Persistence.EntityFramework.Entities;
 using Microsoft.EntityFrameworkCore;
 
 namespace FoodForYou.Persistence.EntityFramework.Migrations.DataSeed
@@ -17,17 +18,31 @@ namespace FoodForYou.Persistence.EntityFramework.Migrations.DataSeed
 
         private static void SeedProducts(ModelBuilder modelBuilder)
         {
-            modelBuilder.Entity<Product>().HasData(new[] { 
-                new Product() {ProductId = 1, ProductName = "Azeite", UnitPrice = 1.20},
-                new Product() {ProductId = 2,  ProductName = "Bolachas", UnitPrice = 1.00},
-                new Product() {ProductId = 3,  ProductName = "Cola", UnitPrice = 2.20},
-                new Product() {ProductId = 4,  ProductName = "Maçã", UnitPrice = 0.15},
-            });
+            modelBuilder.Entity<Product>().HasData(
+                new Product() {ProductId = 1, ProductName = "Olive Oil", UnitPrice = (float) 1.20},
+                new Product() {ProductId = 2, ProductName = "Cookies", UnitPrice = (float) 1.00},
+                new Product() {ProductId = 3, ProductName = "Coke", UnitPrice = (float) 2.20},
+                new Product() {ProductId = 4, ProductName = "Apple", UnitPrice = (float) 0.15}
+                );
         }
 
         private static void SeedEmployees(ModelBuilder modelBuilder)
         {
-            modelBuilder.Entity<Employee>().HasData(new Employee[] { });
+            modelBuilder.Entity<Employee>().HasData(
+                    new Employee() {
+                        EmployeeId = 1,
+                        Title = "CEO",
+                        TitleOfCourtesy = "Mrs.",
+                        Address = "Baker Street 221B",
+                        BirthDate = new DateTime(1990, 12, 10),
+                        City = "London",
+                        Country = "United Kingdom",
+                        FullName = "Scarlet Holmes",
+                        HomePhone = "+351 245234234",
+                        PostalCode = "432-32335",
+                        Region = "West End"
+                    }
+                );
         }
 
         private static void SeedShippers(ModelBuilder modelBuilder)
